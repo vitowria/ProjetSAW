7619f8a156ad60433e01c1d26d15a8cdf01603f9
Vitória Bastos <61765436+vitowria@users.noreply.github.com>
2023-12-09 15:45:45 +0100

    Update Dillution_puls.ino
    Communication serial ok

--- Dillution_puls.ino
+++ Dillution_puls.ino
@@ -1,4 +1,4 @@
-int tension = 0;
+//int tension = 0;
 float starttime;
 float endtime;
 float starttime1;
@@ -6,65 +6,87 @@ float endtime1;
 float starttime2;
 float endtime2;
 
-float concentration_initial = -1; //related to the concentration 
-float concentration_final = -1; //related to the concentration
-
-int stringFlag = -1;
+float t_prime = -1; 
+float t_air = -1;   
+float t_cycle = t_prime + t_air;
 
 bool stringComplete1 = false; 
 bool stringComplete2 = false;
-//serialEvent 
-//Começar com valor impossivel var1 var2
-//setup colocar while alguma das duas com vlaor impossivel
+
+String inputString;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  inputString.reserve(200);
+  //delay(2000); 
+  inputString.reserve(200); 
 
   pinMode(9, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
 
-  while (concentration_initial = -1 || concentration_initial = -1){
-    if(stringComplete1 == true && stringComplete2 == true){
-     concentration_initial = inputString.toFloat(inputString.substring(0, inputString.indexOf(';')));
-     concentration_final = inputString.toFloat(inputString.substring(inputString.indexOf(';')+1, inputString.lastIndexOf(';')));
+  Serial.println("Aguardando dados pela porta serial...");
+  //Serial.println("Aqui1");
+
+  while (true) {
+    while (Serial.available() > 0) {
+      //Serial.println("Aqui2");
+      char inChar = (char)Serial.read();
+      Serial.println(inChar);
+      inputString += inChar;
+
+      if (inChar == ';') {
+        if (!stringComplete1) {
+          stringComplete1 = true;
+        } else {
+          stringComplete2 = true;
+          break;  // Sai do loop assim que receber a segunda parte da string
+        }
+      }
     }
-    else{}
+
+    if (stringComplete1 && stringComplete2) {
+      int index1 = inputString.indexOf(';');
+      int index2 = inputString.lastIndexOf(';');
+
+      if (index1 != -1 && index2 != -1 && index1 < index2) {
+        t_prime = inputString.substring(0, index1).toFloat();
+        t_air = inputString.substring(index1 + 1, index2).toFloat();
+        t_cycle = t_prime + t_air;
+        Serial.println("Dados recebidos com sucesso!");
+        Serial.println("t_prime: " + String(t_prime));
+        Serial.println("t_air: " + String(t_air));
+        Serial.println("t_cycle: " + String(t_cycle));
+        
+        
+        inputString = "";
+        stringComplete1 = false;
+        stringComplete2 = false;
+        //Serial.println("Aqui3");
+        
+        // Sai do loop de setup pra entrar no loop principal
+        break;
+      } else {
+        Serial.println("Erro nos dados. Verifique o formato (tempo1;tempo2;).");
+       
+      }
+    }
+
+    delay(10); 
   }
 }
 
-void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read();
-    // add it to the inputString:
-    inputString += inChar;
-    // if the incoming character is a newline, set a flag so the main loop can
-    // do something about it:
-    if (inChar == ';' && stringComplete1=true) {
-      stringComplete2 = true;
-    }
-    elsif (inChar == ';'){
-      stringComplete1 = true;
-    }
-  }
-}
+
+
 
 
 void loop() {
-  //variavel1 = 180000;
-  //variavel2 = 180000;
-
-  starttime = millis();
-  endtime = starttime;
-  while((endtime - starttime) <= (concentration_initial*0.997)) //pour les COVs
-  {
+    starttime = millis();
+    endtime = starttime;
+    while((endtime - starttime) <= (t_cycle*3000*0.997)) //pour les COVs
+    {
     starttime1 = millis();
     endtime1 = starttime1;
-    while((endtime1 - starttime1) <= 1000)
-    {
+    while((endtime1 - starttime1) <= t_prime*1000){
       digitalWrite(9, LOW);
       digitalWrite(11, HIGH);
       digitalWrite(12, HIGH);
@@ -73,8 +95,8 @@ void loop() {
     
     starttime2 = millis();
     endtime2 = starttime2;
-    while ((endtime2 - starttime2) <= 1000)
-    {
+    
+    while ((endtime2 - starttime2) <= t_air*1000){
       digitalWrite(11, LOW);
       digitalWrite(9, HIGH);
       digitalWrite(12, LOW);
@@ -86,7 +108,7 @@ void loop() {
   }
   starttime = millis();
   endtime = starttime;
-  while((endtime - starttime) <= (concentration_final*0.997))   //pour l'azote
+  while((endtime - starttime) <= (t_cycle*3000*0.997))   //pour l'azote
   {
     digitalWrite(11, LOW);                          
     digitalWrite(9, LOW); 
@@ -94,6 +116,16 @@ void loop() {
     endtime = millis();
     delay(250);
   }
-  
   delay(250);
+  Serial.println("Loop principal concluído. Aguardando novos dados pela porta serial...");
+  while (true) {
+    //Serial.println("Aqui3");
+    delay(20); 
+    //Serial.println("Aqui4");
+    if (Serial.available() > 0) {
+      Serial.println("Aqui5");
+      setup();
+      break;
+    }
+  }
 }
